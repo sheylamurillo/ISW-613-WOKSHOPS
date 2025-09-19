3d737f0d41d08bfe348e1bf53c0c80bf6b14d7da
Sheyla Murillo Cr <programacionprogra81@gmail.com>
2025-09-18 20:21:40 -0600

    Difference between a static file and a dynamic file

---
+++ WORKSHOP-1/index.php
@@ -0,0 +1,9 @@
+<?php
+date_default_timezone_set('America/Costa_Rica');
+
+$fecha = date("d/m/Y"); 
+$hora  = date("H:i:s");   
+
+echo "<p>Fecha actual: $fecha</p>";
+echo "<p>Hora actual: $hora</p>";
+?>
