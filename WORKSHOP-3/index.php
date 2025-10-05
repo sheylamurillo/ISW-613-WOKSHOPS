890669b4e0f09cedbcc4435aba63bd29ee1100f9
Sheyla Murillo Cr <programacionprogra81@gmail.com>
2025-10-05 14:04:44 -0600

    Registro de usuario y login

---
+++ WORKSHOP-3/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WORKSHOP3</title>
+</head>
+
+<body>
+    <form action="save.php" method="post">
+        <label for="name">Name:</label><br>
+        <input type="text" id="name" name="name" required><br><br>
+
+
+        <label for="last-name">Last-name:</label><br>
+        <input type="text" id="last-name" name="last-name" required><br><br>
+
+        <label for="password">Password:</label><br>
+        <input type="password" id="password" name="password" required><br><br>
+
+        <label for="provincia">Provincia:</label><br>
+        <select name="provincia" id="provincia"> 
+           
+            <?php
+              include("save.php"); 
+              leerProvincias(); 
+           ?>
+        </select> 
+
+        
+        <button type="submit">Save</button>
+    </form>
+
+
+</body>
+
+</html>
