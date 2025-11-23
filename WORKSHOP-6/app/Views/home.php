462a23f51e9005525bc9b2841eb7d60fa4a4e9d0
Sheyla Murillo <murillocruzsheyla36@gmail.com>
2025-11-23 16:11:21 -0600

    Formulario de estudiantes y carreras y acciones de crear,actualizar y migracion

---
+++ WORKSHOP-6/app/Views/home.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Users</title>
+</head>
+<body>
+    <ul>
+        <li> 
+            <a href="/users">Users</a> |
+            <a href="/careers">Careers</a>
+        </li>
+       
+    </ul>
+</body>
+</html>
